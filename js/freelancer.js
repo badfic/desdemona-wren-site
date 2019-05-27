02993cbe1e62654416048dd81cf91eaba24eaea5
Badfic Santiago <badfic@outlook.com>
2019-05-27 14:09:49 -0700

    fix blog description spill over

--- js/freelancer.js
+++ js/freelancer.js
@@ -78,14 +78,15 @@
       var el = $(this);
 
       var title = el.find("title").text();
-      var description = el.find("description").text();
+      var descriptionArr = el.find("content\\:encoded").text().split(" ")
+      var descriptionWords = descriptionArr.slice(0, Math.min(descriptionArr.length - 1, 30)).join(" ");
       var link = el.find("link").text();
       var imageUrl = el.find("media\\:thumbnail").attr("url");
 
       if (imageUrl) {
-        $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="' + imageUrl + '"><h4>' + title + '</h4></a><p>' + description + '</p>');
+        $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="' + imageUrl + '"><h4>' + title + '</h4></a><p>' + descriptionWords + '...</p>');
       } else {
-        $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="img/tumblr_logo.png"><h4>' + title + '</h4></a><p>' + description + '</p>');
+        $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="img/tumblr_logo.png"><h4>' + title + '</h4></a><p>' + descriptionWords + '...</p>');
       }
 
       i++;
