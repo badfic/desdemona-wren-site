69c8258470b11819f0ddf8f7853ba879eabf5636
Badfic Santiago <badfic@outlook.com>
2019-07-30 21:42:50 -0700

    temporarily remove blog preview section

--- js/freelancer.js
+++ js/freelancer.js
@@ -72,26 +72,26 @@
     });
   });
 
-  $.get("https://shoppinglist.space/desdemona-wren/blog/feed.rss", function(data) {
-    var i = 1;
-    $(data).find("item").each(function() {
-      var el = $(this);
+//   $.get("https://shoppinglist.space/desdemona-wren/blog/feed.rss", function(data) {
+//     var i = 1;
+//     $(data).find("item").each(function() {
+//       var el = $(this);
 
-      var title = el.find("title").text();
-      var descriptionArr = el.find("content\\:encoded").text().split(" ")
-      var descriptionWords = descriptionArr.slice(0, Math.min(descriptionArr.length - 1, 30)).join(" ");
-      var link = el.find("link").text();
-      var imageUrl = el.find("media\\:thumbnail").attr("url");
+//       var title = el.find("title").text();
+//       var descriptionArr = el.find("content\\:encoded").text().split(" ")
+//       var descriptionWords = descriptionArr.slice(0, Math.min(descriptionArr.length - 1, 30)).join(" ");
+//       var link = el.find("link").text();
+//       var imageUrl = el.find("media\\:thumbnail").attr("url");
 
-      if (imageUrl) {
-        $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="' + imageUrl + '"><h4>' + title + '</h4></a><p>' + descriptionWords + '...</p>');
-      } else {
-        $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="img/tumblr_logo.png"><h4>' + title + '</h4></a><p>' + descriptionWords + '...</p>');
-      }
+//       if (imageUrl) {
+//         $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="' + imageUrl + '"><h4>' + title + '</h4></a><p>' + descriptionWords + '...</p>');
+//       } else {
+//         $('#blog-roll-' + i).append('<a href="' + link + '"><img class="img-fluid" src="img/tumblr_logo.png"><h4>' + title + '</h4></a><p>' + descriptionWords + '...</p>');
+//       }
 
-      i++;
-      return i <= 3;
-    })
-  });
+//       i++;
+//       return i <= 3;
+//     })
+//   });
 
 })(jQuery); // End of use strict
